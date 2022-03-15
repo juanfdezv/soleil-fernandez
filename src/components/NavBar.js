8fff3c238466e6567b4fc7d5583cb92ca394bd30
Juan <juanfernandezv6@gmail.com>
2022-03-14 22:05:36 -0300

    Crear Navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <a className="logo" href="#">Soleil</a>
+            <a href="#">Camperas</a>
+            <a href="#">Camisas</a>
+            <a href="#">Pantalones</a>
+        </nav>
+    );
+}
+
+export default Navbar;
