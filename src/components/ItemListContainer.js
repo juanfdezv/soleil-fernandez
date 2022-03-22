dd7c07ca9b7089b890571e30ac6629a10a4e730c
Juan <juanfernandezv6@gmail.com>
2022-03-21 22:22:39 -0300

    Cambiar ItemListContainer a function based

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import ItemCount from './ItemCount.js';
 
-class ItemListContainer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            items: []
-        };
-    }
-
-    render() {
-        return (
-            <div style={{ paddingTop: 50}}>
-                <ItemCount stock="20" />
-            </div>
-        );
-    }
+const ItemListContainer = (props) => {
+    return (
+        <div style={{ paddingTop: 50}}>
+            <ItemCount stock="20" />
+        </div>
+    );
 }
 
 export default ItemListContainer;
